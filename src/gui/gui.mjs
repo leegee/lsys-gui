90e43a74658391ad8f16f2a51d8be1677586ced1
Lee Goddard <leegee@gmail.com>
2019-05-31 17:57:40 +0200

    Colours. Todo: scale,  bg clr selector init value, canvas size

--- src/gui/gui.mjs
+++ src/gui/gui.mjs
@@ -11,10 +11,7 @@ const Presets = require('./presets.mjs');
 const packageJson = require('../../package.json');
 
 module.exports = class GUI {
-    logFilePath = path.resolve(
-        (process.platform === 'darwin' ? '~/Library/Logs/' : os.homedir() + '/AppData/Roaming/')
-        + electron.remote.app.getName() + '/log.debug'
-    );
+    logFilePath = log.findLogPath();
 
     currentViewName = 'viewMain';
     settings = {
@@ -22,8 +19,8 @@ module.exports = class GUI {
         duration: 48,
         scale: 'pentatonic',
         initialNoteDecimal: 58,
-        canvasWidth: 2000,
-        canvasHeight: 800,
+        canvasWidth: 600,
+        canvasHeight: 400,
         angle: 30,
         xoffset: 0,
         yoffset: 0,
@@ -33,16 +30,19 @@ module.exports = class GUI {
         initX: null,
         initY: null,
         canvasBackgroundColour: '#eeeeee',
+        opacities: [
+            0.8, 0.6, 0.5, 0.4
+        ],
         colours: [
-            "rgba(130,  90, 70, 0.8)",
-            "rgba(33, 180, 24, 0.6)",
-            "rgba(50, 210, 50, 0.5)",
-            "rgba(70, 255, 70, 0.4)"
+            "#825a46",
+            "#21b418",
+            "#32d232",
+            "#46ff46"
         ]
     };
 
     constructor(options) {
-        log.debug('Enter new GUI');
+        log.verbose('Enter new GUI');
         Object.keys(options).forEach(key => {
             this[key] = options[key];
         });
@@ -62,6 +62,15 @@ module.exports = class GUI {
             return collected;
         }, {});
 
+        this.settings.colours.forEach((value, index) => {
+            let el = this.window.document.getElementById('colours-' + index);
+            el.setAttribute('value', value);
+            el.value = value;
+            el = this.window.document.getElementById('opacities-' + index);
+            el.setAttribute('value', this.settings.opacities[index]);
+            el.value = this.settings.opacities[index];
+        });
+
         this.createListeners();
         this.loadPreset();
         this.view(this.currentViewName);
@@ -70,33 +79,35 @@ module.exports = class GUI {
     }
 
     createListeners() {
-        this.window.document.addEventListener('click', (e) => {
+        this.window.document.addEventListener('click', e => {
             if (this.actions[e.target.id]) {
                 this[e.target.id]();
+                return false;
             }
         }, {
                 passive: true
             }
         );
 
-        Array.from(this.elements[this.currentViewName].querySelectorAll('form'))
-            .forEach(
-                form => {
-                    form.addEventListener('change', e => {
-                        log.info(e);
-                        this.settingsChanged(e.target);
-                    }, {
-                            passive: true
-                        }
-                    );
-                }
-            );
+        this.window.document.addEventListener('change', e => {
+            this.settingsChanged(e.target);
+        }, {
+                passive: true
+            }
+        );
     }
 
     settingsChanged(el) {
+        log.debug('settingChanged', el.nodeName);
         if (el.nodeName === 'INPUT') {
-            this.settings[el.id] = el.value.trim();
-            log.silly('INPUT el %s changed to %s: ', el.id, this.settings[el.id], el);
+            const matchGroup = el.id.match(/^(\w+)-(\d+)$/);
+            if (matchGroup) {
+                this.settings[matchGroup[1]][matchGroup[2]] = el.value.trim();
+                log.debug('INTPUT el %o changed %s[%d] to %s', matchGroup[1], matchGroup[2], this.settings[matchGroup[1]][matchGroup[2]]);
+            } else {
+                this.settings[el.id] = el.value.trim();
+                log.silly('INPUT el %o changed to %s: ', el.id, this.settings[el.id], el);
+            }
         }
 
         else {
@@ -166,7 +177,7 @@ module.exports = class GUI {
                         })
                     },
 
-                    { label: '&Preferences', click: () => this.view('viewSettings') },
+                    { label: 'P&references', click: () => this.view('viewSettings') },
                     { role: 'separator' },
                     { role: 'quit' }
                 ]
@@ -223,74 +234,6 @@ module.exports = class GUI {
         this._service.send({ cmd, ...args });
     }
 
-    actionViewMain() {
-        this.view('viewMain');
-    }
-
-    actionGenerate() {
-        log.debug('Enter actionGenerate');
-        this._oldActionGenerate = this.elements.actionGenerate.value;
-        this.elements.actionGenerate.value = 'Generating...';
-        this.elements.actionGenerate.disabled = true;
-        this.elements.actionCreateMidi.disabled = true;
-
-        this.canvas = this.window.document.createElement('canvas');
-        this.ctx = this.canvas.getContext("2d");
-        this.ctx.translate(this.canvas.width / 2, this.canvas.height / 2); // Translate context to center of canvas:
-
-        this.setCanvas();
-
-        this.elements.canvases.insertBefore(this.canvas, this.elements.canvases.firstChild);
-
-        this.x = this.maxX = this.minX = Number(this.settings.initX);
-        this.y = this.maxY = this.minY = Number(this.settings.initY);
-
-        this.lsysSetColour(0);
-
-        log.silly('Call Lsys with', this.settings);
-        this.service('start', this.settings);
-    }
-
-    lsysDone({ content }) {
-        log.debug('Enter lsysDone with %d byes of content', content.length);
-        // if (this.elTimeDisplay) {
-        //     this.elTimeDisplay.innerText = 'Generated in ' + (new Date().getTime() - (elTimeDisplay.get('text'))) + ' ms';
-        // }
-
-        this.canvas.scrollIntoView({
-            behavior: "smooth",
-            block: "end"
-        });
-
-        this.window.document.getElementById('contentDisplay').value = content;
-        this.elements.actionGenerate.value = this._oldActionGenerate;
-        this.elements.actionGenerate.disabled = false;
-        this.elements.actionCreateMidi.disabled = false;
-
-        this.lsysRender(content);
-        this.lsysResize(content);
-
-        this.lsysFinalise();
-    }
-
-    actionGenerateMidi() {
-        const oldValue = this.createMidi.value;
-        this.createMidi.value = 'Hang on...';
-        this.createMidi.disabled = true;
-        fetch('/cgi-bin/fractal_plant_chords.cgi', {
-            duration: this.settings.duration,
-            angle: this.settings.angle,
-            scale: this.settings.scale
-        }).then(() => {
-            this.playSound("/cgi-output/cgi.midi");
-            this.createMidi.value = oldValue;
-            this.createMidi.disabled = false;
-        }).catch(() => {
-            log.error(e);
-            alert('Failure :(');
-        });
-    }
-
     loadPreset(idx = 0) {
         log.info('Load preset ', idx, Presets[idx]);
 
@@ -316,6 +259,81 @@ module.exports = class GUI {
         });
 
         this.window.document.getElementById('title').innerText = Presets[idx].title;
+    }
+
+    actionViewMain() {
+        this.view('viewMain');
+    }
+
+    actionGenerate() {
+        log.verbose('Enter actionGenerate');
+        this._oldActionGenerate = this.elements.actionGenerate.value;
+        this.elements.actionGenerate.value = 'Generating...';
+        this.elements.actionGenerate.disabled = true;
+        this.elements.actionCreateMidi.disabled = true;
+
+        this.canvas = this.window.document.createElement('canvas');
+        this.canvas.addEventListener('click', (e) => this.openElementInNewWindow(e.target));
+        this.ctx = this.canvas.getContext("2d");
+        this.ctx.translate(this.canvas.width / 2, this.canvas.height / 2); // Translate context to center of canvas:
+
+        this.setCanvas();
+
+        this.elements.canvases.insertBefore(this.canvas, this.elements.canvases.firstChild);
+
+        this.x = this.maxX = this.minX = Number(this.settings.initX);
+        this.y = this.maxY = this.minY = Number(this.settings.initY);
+
+        this.preparedColours = [];
+        for (let i = 0; i < this.settings.colours.length; i++) {
+            this.preparedColours[i] = this.hexAndOpacityToRgba(this.settings.colours[i], this.settings.opacities[i])
+        }
+        this.lsysSetColour(0);
+
+        const settings = {
+            start: this.settings.start,
+            variables: this.settings.variables,
+            rules: this.settings.rules,
+            totalGenerations: this.settings.totalGenerations
+        }
+        log.silly('Call Lsys with', settings);
+        this.service('start', settings);
+    }
+
+    actionGenerateMidi() {
+        const oldValue = this.createMidi.value;
+        this.createMidi.value = 'Hang on...';
+        this.createMidi.disabled = true;
+        fetch('/cgi-bin/fractal_plant_chords.cgi', {
+            duration: this.settings.duration,
+            angle: this.settings.angle,
+            scale: this.settings.scale
+        }).then(() => {
+            this.playSound("/cgi-output/cgi.midi");
+            this.createMidi.value = oldValue;
+            this.createMidi.disabled = false;
+        }).catch(() => {
+            log.error(e);
+            alert('Failure :(');
+        });
+    }
+
+    openElementInNewWindow(el) {
+        let win = new electron.remote.BrowserWindow({
+            parent: this.win,
+            modal: true,
+            show: false,
+            backgroundColor: '#000000',
+            width: this.appConfig.gui.modal.width,
+            height: this.appConfig.gui.modal.height
+        });
+        win.setMenu(null);
+        win.loadURL(url.format({
+            protocol: 'file',
+            slashes: true,
+            pathname: path.join(__dirname, viewName + '.html')
+        }));
+        win.once('ready-to-show', () => win.show());
     }
 
     lsysResize(content) {
@@ -369,10 +387,9 @@ module.exports = class GUI {
                     break;
                 case 'c': // Set colour
                     const colourCode = content.charAt(++i);
-                    log.silly('Got colour code (%s)', colourCode);
-                    this.lsysSetColour(
-                        parseInt(colourCode, 10) % this.settings.colours
-                    );
+                    const index = parseInt(colourCode, 10) % this.settings.colours.length;
+                    log.silly('Got colour code (%s) made index', colourCode, index);
+                    this.lsysSetColour(index);
                     draw = false;
                     break;
                 case '+': // Right
@@ -405,7 +422,7 @@ module.exports = class GUI {
     }
 
     lsysTurtleGraph(dir) {
-        log.debug('Move dir (%s) from x (%s) y (%s)', dir, this.x, this.y);
+        log.silly('Move dir (%s) from x (%s) y (%s)', dir, this.x, this.y);
 
         this.ctx.beginPath();
         if (this.settings.generationsScaleLines > 0) {
@@ -434,7 +451,7 @@ module.exports = class GUI {
         if (this.x < this.minX) this.minX = this.x;
         if (this.y < this.minY) this.minY = this.y;
 
-        log.debug('Moved to x (%s) y (%s)', this.x, this.y);
+        log.silly('Moved to x (%s) y (%s)', this.x, this.y);
     };
 
     setWidth(px) {
@@ -444,15 +461,26 @@ module.exports = class GUI {
     lsysFinalise() {
         log.verbose('Enter lsysFinalise');
         if (this.settings.finally && typeof this.settings.finally === 'function') {
-            log.debug('Call finally');
+            log.verbose('Call finally');
             this.settings.finally.call(this);
         }
         log.verbose('Leave lsysFinalise');
     };
 
+    // Thanks https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+    hexAndOpacityToRgba(hex, opacity) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? 'rgb(' +
+            parseInt(result[1], 16) + ',' +
+            parseInt(result[2], 16) + ',' +
+            parseInt(result[3], 16) + ',' +
+            opacity +
+            ')' : null;
+    }
+
     lsysSetColour(colourIndex) {
-        this.colour = this.settings.colours[colourIndex];
-        log.debug('Set colour to index (%d): ', colourIndex, this.colour);
+        this.colour = this.preparedColours[colourIndex];
+        log.silly('Set colour to index (%d): ', colourIndex, this.colour, this.settings.colours);
         this.ctx.strokeStyle = this.colour;
     }
 
@@ -461,5 +489,27 @@ module.exports = class GUI {
         this.canvas.height = this.settings.canvasHeight;
         this.ctx.fillStyle = this.settings.canvasBackgroundColour;
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    lsysDone({ content }) {
+        log.verbose('Enter lsysDone with %d byes of content', content.length);
+        // if (this.elTimeDisplay) {
+        //     this.elTimeDisplay.innerText = 'Generated in ' + (new Date().getTime() - (elTimeDisplay.get('text'))) + ' ms';
+        // }
+
+        this.canvas.scrollIntoView({
+            behavior: "smooth",
+            block: "end"
+        });
+
+        this.window.document.getElementById('contentDisplay').value = content;
+        this.elements.actionGenerate.value = this._oldActionGenerate;
+        this.elements.actionGenerate.disabled = false;
+        this.elements.actionCreateMidi.disabled = false;
+
+        this.lsysRender(content);
+        this.lsysResize(content);
+
+        this.lsysFinalise();
     }
 }
