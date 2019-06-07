50649e5aaaa4cfbc25d391d0b48a700ef4bafe6f
Lee Goddard <leegee@gmail.com>
2019-06-07 12:28:54 +0200

    coverage

--- src/MIDI.spec.js
+++ src/MIDI.spec.js
@@ -5,12 +5,15 @@ const MIDI = require("./MIDI.mjs");
 
 describe('MIDI', () => {
     [
-        [10, 10]
+        [10, 10],
+        [0, 127],
+        [126, 127],
+        [1, 1]
     ].forEach(([lowestNote, highestNote]) => {
-        it('pitchOffset', () => {
-            expect(
-                MIDI.pitchOffset(lowestNote, highestNote)
-            ).to.be.within(0, 127);
-});
+        it('pitchOffset ' + lowestNote + '-' + highestNote, () => {
+            const offset = MIDI.pitchOffset(lowestNote, highestNote);
+            console.log('offset ', offset);
+            expect(offset).to.be.within(0, 127);
+        });
     });
 });
